47826048281d28a05d89977a1ecbf68e7e1c1739
Godspower <petersgodspower95@gmail.com>
2024-08-09 10:12:59 +0100

    cocmmit on the main.jsx file

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -7,20 +7,20 @@ function Main() {
   const [articles, setArticles] = useState([]);
 
   const handleChange = (e) => {
-    switch(e.target.value) {
-        case "business":
-         fetchNews('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=db87e9cd162147e4968361973325e619');
-         break;
-         case "world":
+    switch (e.target.value) {
+      case "business":
+        fetchNews('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=db87e9cd162147e4968361973325e619');
+        break;
+      case "world":
         fetchNews('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=db87e9cd162147e4968361973325e619');
         break;
-        case "tech":
+      case "tech":
         fetchNews('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=db87e9cd162147e4968361973325e619');
         break;
-        case "tesla":
+      case "tesla":
         fetchNews('https://newsapi.org/v2/everything?q=tesla&from=2024-07-07&sortBy=publishedAt&apiKey=db87e9cd162147e4968361973325e619');
         break;
-        default:
+      default:
         fetchNews('https://newsapi.org/v2/top-headlines?country=us&apiKey=db87e9cd162147e4968361973325e619');
         break;
     }
@@ -34,40 +34,43 @@ function Main() {
     try {
       let response = await fetch(link);
       let data = await response.json();
-      setArticles(data.articles);
+      if (data.articles) {
+        setArticles(data.articles);
+      } else {
+        setArticles([]); // Set an empty array if articles are not present
+      }
     } catch (error) {
       console.error(error);
+      setArticles([]); // Set an empty array on error
     }
   };
 
   const refresh = () => {
     const randomNumber = Math.floor(Math.random() * links.length);
     const randomLink = links[randomNumber];
-
     fetchNews(randomLink);
   }
 
   return (
-   <div className="main-container">
-     <Header handleChange={handleChange} />
-     
-     <div id='news-container'>
-      {articles.map((article, index) => (
-        <News
-          key={index}
-          author={article.author}
-          description={article.description}
-          title={article.title}
-          url={article.url}
-          urlToImage={article.urlToImage}
-          publishedAt={article.publishedAt}
-        />
-      ))}
+    <div className="main-container">
+      <Header handleChange={handleChange} />
+      <div id='news-container'>
+        {articles.map((article, index) => (
+          <News
+            key={index}
+            author={article.author}
+            description={article.description}
+            title={article.title}
+            url={article.url}
+            urlToImage={article.urlToImage}
+            publishedAt={article.publishedAt}
+          />
+        ))}
+      </div>
+      <article className='button-holder'>
+        <button id='refresh' onClick={refresh}>Refresh</button>
+      </article>
     </div>
-    <article className='button-holder'>
-    <button id='refresh' onClick={refresh}>Refresh</button>
-    </article>
-   </div>
   );
 }
 
